14abbea13ec4246450a7d4a91e4682febfffade1
samushika-chamini <42279795+samushika-chamini@users.noreply.github.com>
2019-03-26 06:40:02 +0530

    Update and rename frog.java to Vickey.java

--- Vickey.java
+++ Vickey.java
@@ -1,16 +1,18 @@
-public class main {
+public class Vickey {
     public static void main(String[] args){
         int[] jump = {5, 3, 2 };
         int[] interval = {2, 3, 5 };
         int x = 0;
-        int inputs = 17;   //Distance that supposed to travel tests : 10, 11 , 15, 17, 20 , 22
+	    int count=0;
+        int inputs = 22;   
         int time = 0;
         int distance = 0;
         System.out.println("Steps = ");
         while(x < inputs) {
             for (int i = 0; i < jump.length; ++i){
                 System.out.println(jump[i]);
-                x += jump[i];
+                x += jump[i];		
+		        count++;
                 if (x < inputs){
                     time += interval[i];
                 } else {
@@ -26,7 +28,8 @@ public class main {
 
         }
 
-        System.out.println("Total Distance = " + distance);
+        //System.out.println("Total Distance = " + distance);
         System.out.println("Total waiting time = " + time);
+ 	    System.out.println("Count = " + count);
     }
 }
